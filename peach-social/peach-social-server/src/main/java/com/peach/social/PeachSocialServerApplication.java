fd8edb153eb17ec065d87bc233ae5130aebd8b92
liyida <1020362187@qq.com>
2020-12-22 01:02:48 +0800

    新建项目

---
+++ peach-social/peach-social-server/src/main/java/com/peach/social/PeachSocialServerApplication.java
@@ -0,0 +1,13 @@
+package com.peach.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeachSocialServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeachSocialServerApplication.class, args);
+	}
+
+}
---
+++ peach-social/peach-social-server/src/test/java/com/peach/social/PeachSocialServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peach.social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeachSocialServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
